ff12cc92a7c1d64a9d7ee0f1df4e6a969155449c
TomerAmran <50573231+TomerAmran@users.noreply.github.com>
2020-01-20 12:39:09 +0200

    updates

--- initiate.py
+++ initiate.py
@@ -1,13 +1,15 @@
-import os
-os.remove('moncafe.db') # delete DataBase
-from Repository import repo
-from DAO import Coffee_stands, Activities, Employees, Suppliers
-from DTO import Employee
-
 if __name__ == '__main__':
+    import os
+    # delete DataBase if exists
+    if os.path.exists('moncafe.db'):
+        os.remove('moncafe.db')
+    # imports
+    from Repository import repo
+    from DAO import Coffee_stands, Activities, Employees, Suppliers
+    from DTO import Employee
     with open('config.txt') as inputFile:
         for line in inputFile:
             print(line)
-    rep.employees.insert(Employee(1, 'Tomer', 35, 1))
-    rep.conn.execute('SELECT * FROM employees')
+    repo.employees.insert(Employee(1, 'Tomer', 35, 1))
+    repo.conn.execute('SELECT * FROM employees')
     print(rep.conn.fetchAll())
